7a198af86e60595082002f1bf1a4941724ea4609
Andrew-Junkin <adjunkin@gmail.com>
2021-07-21 13:44:30 -0400

    pushing files

---
+++ client/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div class="jumbotron jumbotron-fluid bg-gray">
+        <div class="text-center container">
+            <h1 class="display-4">Fluid jumbotron</h1>
+            <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+        </div>
+        </div>
+    )
+}
+
+export default Header;
